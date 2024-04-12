dc7c6676e557b924b905c42fe7a5c649b11cb65d
Aman <amanraj89969@gmail.com>
2024-04-12 23:26:31 +0530

    adding bootstrap navbar and minor fixes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
-document.getElementById('resume').addEventListener('click', function() {
-    window.location.href = 'https://drive.google.com/file/d/1YcYZaL3ArwH8FYSUafOQm9hKjVlAr5kP/view?usp=drive_link';
+document.getElementById("resume").addEventListener("click", function () {
+  window.location.href =
+    "https://drive.google.com/file/d/1YcYZaL3ArwH8FYSUafOQm9hKjVlAr5kP/view?usp=drive_link";
 });
